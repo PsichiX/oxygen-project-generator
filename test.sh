caecc6c15279c25d979b9885b04f75628b9c0f20
Patryk 'PsichiX' Budzynski <psichix@gmail.com>
2018-05-31 19:13:45 +0200

    travis ci support added

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd tests
+mkdir app
+mkdir lib
+cd app
+node ../../index.js -n app
+node ../../index.js -n app -i
+node ../../index.js -n app -i -b
+cd ../lib
+node ../../index.js -n lib --lib
+node ../../index.js -n lib --lib -i
+node ../../index.js -n lib --lib -i -b
